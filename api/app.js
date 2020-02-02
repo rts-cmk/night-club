b58951b34c1d420a5577e13c09fb51d068c751b2
Brian Emilius <brian@brianemilius.com>
2020-02-02 19:13:13 +0100

    add cors support

--- api/app.js
+++ api/app.js
@@ -3,9 +3,11 @@ var express = require("express");
 var app = express();
 var router = require("./router");
 var { testConnection } = require("./config/database");
+var cors = require("cors");
 
 testConnection();
 
+app.use(cors());
 app.use(router);
 
 module.exports = app;
